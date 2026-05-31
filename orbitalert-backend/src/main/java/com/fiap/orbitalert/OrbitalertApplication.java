488b8d7c7b26ab55e408ae83de703ef2811e1f54
kondrasz <kondrasz@192.168.1.20>
2026-05-31 14:37:25 -0300

    chore: estrutura inicial do backend gerada pelo Spring Initializr com Web, JPA e H2

---
+++ orbitalert-backend/src/main/java/com/fiap/orbitalert/OrbitalertApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.orbitalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitalertApplication.class, args);
+	}
+
+}
